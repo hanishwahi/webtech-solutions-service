97cb8d1e80b198c636403a76f514fe20eb3c016f
webtech <purposeresearch923@gmail.com>
2023-07-18 11:49:40 +0530

    commit

--- src/Pages/Blog/BlogDetails.js
+++ src/Pages/Blog/BlogDetails.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import { data } from './BlogsData'
-// import { useEffect } from 'react'
-
+ 
 
 
 function BlogDetails() {
@@ -12,37 +11,38 @@ function BlogDetails() {
   const { id } = useParams();
   const currentId = id
 
-  console.log(typeof currentId, currentId)
-
-
-  // useEffect(() => {
-  //   fetch('https://dummyjson.com/posts').then((res) => {
-  //     res.json().then((result) => {
-  //       setBlogDetails(result.posts)
-  //     })
-  //   })
-  // }, [])
+  const Navigate= useNavigate();
+  
+  function backWindow(){
+    Navigate(-1)
+  }
+ 
 
   const newblog = blogDetails[currentId - 1]
-  console.log(typeof newblog, newblog)
+  // console.log(typeof newblog, newblog)
 
   return (
     <>
       <Header />
       <div className='container-fluid py-5'>
         <div className='container-lg'>
-          <div className='row'>
-            {newblog && <h2 className='h5 mb-3' style={{ color: "#42ade4" }}><span >Posted on: </span>  {newblog.date}</h2>}
+          <div className='row align-items-center  mb-3'>
+            <div className='col-lg-6 col-8 blog-detail'>
+              {newblog && <h2><span >Posted on: </span>  {newblog.date}</h2>}
+            </div>
+            <div className='col-lg-6 col-4 text-end blog-detail'>
+               <Link onClick={backWindow}>Back</Link>
+            </div>
           </div>
           <div className='row mb-3'>
             <div className='col'>
               <img className='img-fluid p-0' width={500} src={newblog.image} alt='' />
             </div>
           </div>
-          
+
 
           <div className='row'>
-            {newblog && <h2 className='h3 mb-3'style={{ color: "#42ade4" }}>{newblog.title}?</h2>}
+            {newblog && <h2 className='h3 mb-3' style={{ color: "#42ade4" }}>{newblog.title}?</h2>}
           </div>
 
           <div className='row blog-detail'>
