59f8ed5bda3eb75a1f7881bf9697fc26dc80b971
webtech <purposeresearch923@gmail.com>
2023-07-18 10:15:00 +0530

    commit

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,21 +3,34 @@ import Banner from './Banner'
 import OurMission from './OurMission'
 import CounterRecord from '../Home/CounterRecord'
 import Unique from './Unique'
-import WhyUs from './WhyUs' 
+import WhyUs from './WhyUs'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import { Helmet } from 'react-helmet';
+
 
 
 function About() {
   return (
     <>
-         <Header/>
-            <Banner/>
-            <OurMission/>
-            <CounterRecord/>
-            <Unique/>
-            <WhyUs/>
-            <Footer/>
+      <div>
+        <Helmet>
+          <title>About</title>
+          <meta name="description" content="Your page description" />
+          <meta name="keywords" content="keyword1, keyword2, keyword3" />
+          <link rel="canonical" href="https://www.example.com/your-page" />
+          {/* Add more meta tags as needed */}
+        </Helmet>
+
+        <Header />
+        <Banner />
+        <OurMission />
+        <CounterRecord />
+        <Unique />
+        <WhyUs />
+        <Footer />
+      </div>
+
     </>
   )
 }
