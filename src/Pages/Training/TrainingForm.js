fd8e823254d5a6088380a98047ead00d5a69c386
webtech <purposeresearch923@gmail.com>
2023-07-27 09:56:52 +0530

    commit

--- src/Pages/Training/TrainingForm.js
+++ src/Pages/Training/TrainingForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import emailjs from 'emailjs-com';
-import { useRef} from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import {useRef} from 'react';
+import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function TrainingForm() {
@@ -9,17 +9,11 @@ function TrainingForm() {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs
-            .sendForm('service_hmnkl0m', 'template_x63rd3m', e.target, 'DaUmEfgqFFS3JD4_d')
-            .then(
-                (result) => {
-                    console.log(result.text);
-                },
-                (error) => {
-                    console.log(error.text);
-                }
-
-            );
+        emailjs.sendForm('service_hmnkl0m', 'template_x63rd3m', e.target, 'DaUmEfgqFFS3JD4_d').then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
 
         e.target.reset();
         toast.success('Message sent Successfully!');
@@ -28,26 +22,27 @@ function TrainingForm() {
 
     return (
         <>
-            <div>
+            <div>                  
                 <div className='row graphic-desg-heading pb-lg-4 pb-2 border-new'>
                     <h1 className='px-0'>Book Your Seat</h1>
                 </div>
                 <div className='row'>
-                    <form ref={form} onSubmit={sendEmail}>
+                    <form ref={form}
+                        onSubmit={sendEmail}>
                         <div className='row justify-content-between'>
                             <div className="form-outline col-12 col-sm-6 mb-4">
                                 <label className="form-label fw-bold" htmlFor="form4Example1">*Name</label>
-                                <input type="text" name="name" id="form4Example1" required className="form-control" placeholder='Full Name' />
+                                <input type="text" name="name" id="form4Example1" required className="form-control" placeholder='Full Name'/>
                             </div>
 
                             <div className="form-outline col-12 col-sm-6 mb-4">
                                 <label className="form-label fw-bold" htmlFor="form4Example2">*Phone</label>
-                                <input type="number" name="phone" id="form4Example2" required className="form-control" placeholder='Enter your Phone number' />
+                                <input type="number" name="phone" id="form4Example2" required className="form-control" placeholder='Enter your Phone number'/>
                             </div>
                         </div>
                         <div className="form-outline mb-4">
                             <label className="form-label fw-bold" htmlFor="form4Example2">*Email address</label>
-                            <input type="email" name="email" id="form4Example2" required className="form-control" placeholder='Enter your Email ID' />
+                            <input type="email" name="email" id="form4Example2" required className="form-control" placeholder='Enter your Email ID'/>
                         </div>
                         <div className="form-outline mb-4">
                             <label className="form-label fw-bold" htmlFor="form4Example2">Service</label>
@@ -65,12 +60,12 @@ function TrainingForm() {
                         </div>
 
 
-                        <input className='form-Button' type="submit" value="Submit" />
+                        <input className='form-Button' type="submit" value="Submit"/>
                     </form>
                 </div>
             </div>
 
-            <ToastContainer />
+            <ToastContainer/>
         </>
     )
 }
