b8f73cee6b54c0fde117736c0f7622b14d4be7ea
webtech <purposeresearch923@gmail.com>
2023-08-09 12:53:28 +0530

    commit

--- src/Pages/Career/Jobs.js
+++ src/Pages/Career/Jobs.js
@@ -11,12 +11,11 @@ function Jobs() {
                         <p>We want to make agency life peasant, productive and empowering</p>
                     </div>
                     <div className='row'>
-
                         <div className='col-lg-12 mb-4'>
                             <div className="accordion" id="accordionExample">
                                 <div className="accordion-item">
                                     <h2 className="accordion-header" id="headingOne">
-                                        <button className="accordion-button shadow-none fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        <button className="accordion-button shadow-none fw-bold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                                             SEO Executive - Immediate Joining
                                         </button>
                                     </h2>
@@ -56,7 +55,57 @@ function Jobs() {
                             </div>
                         </div>
                     </div>
+                    <div className='row'>
+                        <div className='col-lg-12 mb-4'>
+                            <div className="accordion" id="accordionExample">
+                                <div className="accordion-item">
+                                    <h2 className="accordion-header" id="headingTwo">
+                                        <button className="accordion-button shadow-none fw-bold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseOne">
+                                            Project Manager
+                                        </button>
+                                    </h2>
+                                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                        <div className="accordion-body pb-0">
+                                            <div className='mb-3'>
+                                                <h1 className='h5'>Job description:</h1>
+                                                <p>Project Coordinator role allows to be in charge of various projects to plan, budget, oversee, document and manage all aspects related to the specific project. Manage the delivery of internal and outsourced projects from project initiation to transition to operations. Deliver and execute projects on time within budget to meet the desired business objectives.</p>
+                                            </div>
+                                            <div className='mb-4'>
+                                                <h1 className='h5'>Roles And Responsibility:</h1>
+                                                <li>Maintaining and monitoring project plans, project schedules, work hours, budgets, and expenditures.</li>
+                                                <li>Organizing, attending, and participating in clients meetings.</li>
+                                                <li>Documenting and following up on important actions and decisions from meetings.</li>
+                                                <li>Ensuring project deadlines are met.</li>
+                                                <li>Determining project changes.</li>
+                                                <li>Providing administrative support as needed.</li>
+                                                <li>Undertaking project tasks as required.</li>
+                                                <li>Developing project strategies.</li>
+                                                <li>Ensuring projects adhere to frameworks and all documentation is maintained appropriately for each project.</li>
+                                                <li>Assess project risks and issues and provide solutions where applicable.</li>
+                                                <li>Ensure stakeholder views are managed towards the best solution.</li>
+                                                <li>Chair and facilitate meetings where appropriate and distribute minutes to all project team members.</li>
+                                                <li>Create a project management calendar for fulfilling each goal and objective.</li>
+                                            </div>
+                                            <div className="mb-4">
+                                           <p> <strong>Notes :</strong> Excellent Communication , Project &Service Management Skills.</p>
+                                            </div>
+                                             
+                                            <div>
+                                                <p className='mb-1'><strong>Experience :</strong>  2-4 year </p>
+                                                <p className='mb-1'> <strong>Location :</strong>  Mohali</p>
+                                                <p className='mb-1'><strong>Interested Candidate call at :</strong><Link to="tel:+91 8427682603"> +91 8427682603</Link></p>
 
+                                            </div>
+                                            
+                                        </div>
+                                        <div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
