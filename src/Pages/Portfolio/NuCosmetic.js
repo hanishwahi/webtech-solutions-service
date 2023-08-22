9d98999d089dddcc0610b49326cf09f15611686f
webtech <purposeresearch923@gmail.com>
2023-08-22 16:46:19 +0530

    commit

--- src/Pages/Portfolio/NuCosmetic.js
+++ src/Pages/Portfolio/NuCosmetic.js
@@ -1,7 +1,6 @@
 import React from 'react'
 // import Carousel from "react-multi-carousel";
 // import "react-multi-carousel/lib/styles.css";
- 
 import nuCosmeticCarousel from '../../Images/NuCosmeticCarousel.webp'
 import nuCosmeticCarouselWall from '../../Images/Nu CosmeticCaraouselWall.webp'
 
