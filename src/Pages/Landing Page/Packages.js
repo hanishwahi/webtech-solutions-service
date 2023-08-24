1733c75bbccc7fa1e8748b6c19b09e7be8b0a27a
webtech <purposeresearch923@gmail.com>
2023-08-24 17:49:55 +0530

    commit

--- src/Pages/Landing Page/Packages.js
+++ src/Pages/Landing Page/Packages.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import off from './../../Images/packageoff (1).png'
+import off2 from './../../Images/packageoff (2).png'
 
 function Packages() {
     return (
@@ -6,22 +8,22 @@ function Packages() {
             <div className="container-fluid py-lg-5 py-3">
                 <div className="container-xl pt-lg-5 pt-3">
                     <div className="row packages-main">
-                        <h1>SOLUTIONS WE OFFER</h1>
-                        <p>Choose a plan tailored to your needs</p>
+                        <h1>Packages we offer</h1>
                     </div>
                     <div className="row py-4">
                         <div className="col-lg-4 col-sm-6 col-md-6 mb-3">
                             <div className='shadow package-plan'>
                                 <div className='border-bottom'>
-                                    <h3>Silver</h3>
-                                    <p>Customized anything in anytime</p>
+                                    <h3>Digital Marketing </h3>
                                 </div>
                                 <div>
                                     <ul>
-                                        <li>Website Design and Development</li>
-                                        <li>Social Media Marketing & Search Engine Optimization</li>
-                                        <li>Graphic Design Services: & Package</li>
-                                        <li>Ongoing Support and Maintenance:</li>
+                                        <li>SEO</li>
+                                        <li>Technical SEO</li>
+                                        <li>Google My Business </li>
+                                        <li>Social Media Marketing</li>
+                                        <li>Pay Per Click (PPC)</li>
+                                        <li>E-Commerce Marketing</li>
                                     </ul>
                                 </div>
                             </div>
@@ -29,36 +31,35 @@ function Packages() {
                         <div className="col-lg-4 col-sm-6 col-md-6 mb-3">
                             <div className='shadow package-plan'>
                                 <div className='border-bottom'>
-                                    <h3>Gold</h3>
-                                    <p>Establish strong online presence</p>
+                                    <h3>Web Development</h3>
                                 </div>
                                 <div>
                                     <ul>
-                                        <li>Website Design and Development</li>
-                                        <li>Social Media Marketing & Search Engine Optimization</li>
-                                        <li>Graphic Design Services: & Package</li>
-                                        <li>Ongoing Support and 1 Year Maintenance:</li>
+                                        <li>UI/UX Designing</li>
+                                        <li>Frontend Development</li>
+                                        <li>Backend Development</li>
+                                        <li>Wordpress/Shopify Development</li>
+                                        <li>Maintenance & Support</li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-4 col-sm-6 col-md-6 mb-3">
                             <div className='shadow package-plan'>
-                                <div className='border-bottom'>
-                                    <h3>Platinum</h3>
-                                    <p>Customized anything in anytime</p>
+                                <div className='row border-bottom'>
+                                    <h3>COMBO</h3>
                                 </div>
-                                <div>
-                                    <ul>
-                                        <li>Website Design and Development</li>
-                                        <li>Social Media Marketing & Search Engine Optimization</li>
-                                        <li>Graphic Design Services: & Package</li>
-                                        <li>24/7 Online Support</li>
-                                    </ul>
+ 
+                                <div className="row p-5 justify-content-center">
+                                        <img width={140} src={off} alt="" />
+                                </div>
+
+                                <div className="row align-items-center"> 
+                                    <h3>  Digital Marketing <br /> + <br />Web Development</h3>
                                 </div>
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
